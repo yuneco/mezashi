1f288bff739440d3f69e610250c52ae7ec14f57a
yuneco <yukiyuneco@gmail.com>
2019-02-20 21:39:59 +0900

    Add Mezashi on the Stage

--- src/components/GameStage.vue
+++ src/components/GameStage.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="stage-root">
+    <div class="cont">
+      <button @click="jumpPlayer">Jump</button>
+      <button @click="fireMezashi">Mezashi</button>
+    </div>
+
     <cat v-for="cat in cats" ref="cat" :key="`cat-${cat.id}`"
       :x="cat.pos.x" :y="cat.pos.y" :s="cat.pos.s"
       @click="catClicked(cat)"
     ></cat>
 
+    <mezashi v-for="mezashi in mezashis" ref="mezashi" :key="`mezashi-${mezashi.id}`"
+      :x="mezashi.pos.x" :y="mezashi.pos.y" :s="mezashi.pos.s"
+      @hitCat="onMezashiHitCat"
+    ></mezashi>
+
     <player ref="player"
       :x="player.pos.x" :y="player.pos.y" :s="player.pos.s"
-      @hitCat="onHitCat"
+      @hitCat="onPlayerHitCat"
     ></player>
 
   </div>
@@ -29,12 +39,13 @@
 <script>
 import Cat from '@/components/charas/Cat'
 import Player from '@/components/charas/Player'
+import Mezashi from '@/components/charas/Mezashi'
 import Tween from '@/core/Tween'
 import CollisionDetector from '@/core/CollisionDetector'
 
 export default {
   name: 'GameStage',
-  components: { Cat, Player },
+  components: { Cat, Player, Mezashi },
   props: {
     width: { type: Number, default: 320 },
     height: { type: Number, default: 568 }
@@ -45,6 +56,8 @@ export default {
       collisionDetector: new CollisionDetector(),
       cats: [
         { id: 0, pos: { x: 200, y: this.height - 20, s: 0.2 } }
+      ],
+      mezashis: [
       ],
       player: {
         pos: { x: 40, y: this.height - 20, s: 0.2 }
@@ -62,8 +75,33 @@ export default {
       this.collisionDetector.add(this.$refs.player, ...this.$refs.cat)
       this.collisionDetector.detect()
     },
-    onHitCat (catComp) {
-      console.log('HIT CAT', catComp)
+    onPlayerHitCat (catComp) {
+      console.log('PLAYER HIT CAT', catComp)
+    },
+    onMezashiHitCat (catComp) {
+      console.log('MEZASHI HIT CAT', catComp)
+    },
+    async fireMezashi () {
+      const id = Math.floor(Math.random() * 100000)
+      const mz = { id, pos: { x: 100, y: this.height - 20, s: 0.2 } }
+      // Decide Y pos of mezashi
+      const player = this.$refs.player
+      const ppos = player.$el.getBoundingClientRect()
+      mz.pos.y = ppos.y + (ppos.height * 0.55)
+      this.$data.mezashis.push(mz)
+      // animation
+      mz.tw = new Tween(mz.pos)
+      await mz.tw.to({ x: this.width + 100 }, 2000)
+      this.removeMezashi(mz)
+    },
+    removeMezashi (mezashiData) {
+      const index = this.$data.mezashis.indexOf(mezashiData)
+      if (index === -1) { return }
+      this.$data.mezashis.splice(index, 1)
+    },
+    jumpPlayer () {
+      const player = this.$refs.player
+      player.jump()
     }
   },
   mounted () {
---
+++ src/components/charas/Mezashi.vue
@@ -0,0 +1,29 @@
+<template>
+  <e-cont class="mezashi-root"
+    :x="x - 66" :y="~~y + dpos.y - 16" :w="132" :h="32" :r="r"
+    :s="[s * dpos.sx, s * dpos.sy]"
+    :ox="66" :oy="16"
+  >
+      <mezashi-svg></mezashi-svg>
+    </e-cont>
+</template>
+
+<script>
+import ECont from '@/components/core/ECont'
+import MezashiSvg from '@/assets/Mezashi.svg'
+export default {
+  name: 'Mazashi',
+  components: { ECont, MezashiSvg },
+  props: {
+    x: { type: [Number, String], default: 0 },
+    y: { type: [Number, String], default: 0 },
+    r: { type: [Number, String], default: 0 },
+    s: { type: [Number, String], default: 1 }
+  },
+  data () {
+    return {
+      dpos: { y: 0, sx: 1, sy: 1 }
+    }
+  }
+}
+</script>
