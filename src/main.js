5b52cc12c990450046dbd14d9c9aacc0423eef2d
yuneco <yukiyuneco@gmail.com>
2019-02-21 21:46:25 +0900

    Add static OGP

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+if (process.env.NODE_ENV === 'production') {
+  // activate GA only under the release env
+  Vue.use(VueAnalytics, {
+    id: 'UA-121209972-2',
+    router
+  })
+}
 
 new Vue({
   router,
